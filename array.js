3b479c5914d20f46d07b08ff875ad942cb6b9ca3
Syhan Loc <syhanloc@gmail.com>
2017-11-08 16:32:20 +0000

    first commit

---
+++ array.js
@@ -0,0 +1,9 @@
+ var todos = ['item 1', 'item 2', 'item 3']
+ 
+ 
+ todos.push('item 4');
+ 
+ todos.push('item 5');
+ 
+ console.log('My todo: ', todos);
+ todos[2]
---
+++ array2.js
@@ -0,0 +1,7 @@
+var todos = ['item 1 updated', 'item 2', 'item 3', 'item 4', 'item 5'];
+
+todos.splice(0, 1)
+
+todos.splice(3,1)
+
+console.log(todos);
